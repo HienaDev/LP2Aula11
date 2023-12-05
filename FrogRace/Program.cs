7b54f73c37884632ea81f2d6caeaa5dc15ec4376
HienaDev <ant.rodrigues1999@gmail.com>
2023-12-05 15:24:10 +0000

    Add projects

---
+++ FrogRace/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace FrogRace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread T_One = new Thread(FrogJump);
+            T_One.Name = "T_One";
+
+            Thread T_Two = new Thread(FrogJump);
+            T_Two.Name = "T_Two";
+
+            Thread T_Three = new Thread(FrogJump);
+            T_Three.Name = "T_Three";
+
+            T_One.Start(1);
+            T_Two.Start(2);
+            T_Three.Start(3);
+            T_One.Join();
+            T_Two.Join();
+            T_Three.Join();
+        }
+
+        public static void FrogJump(object obj)
+        {
+            Random rng = new Random((int)obj);
+
+            for (int jump = 1; jump < 11; jump++)
+            {
+
+                Thread.Sleep((int)(rng.NextSingle() * 1000));
+
+                Console.WriteLine($"Rã {obj} deu salto {jump} (thread {Thread.CurrentThread.Name})");
+   
+            }
+
+            Console.WriteLine($"Rã {obj} terminou!");
+            
+        }
+    }   
+}
